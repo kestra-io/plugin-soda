af2b84ba6e0be31f61a678b71e92bad511aac61e
Ludovic DEHON <tchiot.ludo@gmail.com>
2025-01-06 23:51:02 +0100

    chore: adapt to latest taskrunner specs

--- src/main/java/io/kestra/plugin/soda/AbstractSoda.java
+++ src/main/java/io/kestra/plugin/soda/AbstractSoda.java
@@ -66,7 +66,7 @@ public abstract class AbstractSoda extends Task {
     @PluginProperty
     @Builder.Default
     @Valid
-    private TaskRunner taskRunner = Docker.instance();
+    private TaskRunner<?> taskRunner = Docker.instance();
 
     @Schema(title = "The task runner container image, only used if the task runner is container-based.")
     @PluginProperty(dynamic = true)
